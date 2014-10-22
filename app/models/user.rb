b8e65af12b6e7b9e463e2c9b35fd4b5bbfc424a4
Michael Teevan <michael.teevan@gmail.com>
2014-10-22 16:37:50 -0700

    user authentication mostly working

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,25 @@ class User < ActiveRecord::Base
   include BCrypt
   has_many :comments
 
+  validates_presence_of :firstname, :message => "First name must be present"
+
+  validates_presence_of :lastname, :message => "Last name must be present"
+
+  validates_presence_of :email, :message => "Email must be present"
+
+  validates_presence_of :password, :message => "Password name must be present"
+
+  # validates :firstname, presence: true, length: { maximum: 50 }, :message => "First name must be present"
+  # validates :lastname, presence: true, length: { maximum: 50 }, :message => "Last name must be present"
+
+
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, presence: true,
+  #           format: { with: VALID_EMAIL_REGEX },
+  #           uniqueness: { case_sensitive: false }, :message => "something"
+  # validates :password, presence: true, length: { minimum: 6 }, :message => "Password must be at least six characters"
+  # validates :password_confirmation, presence: true
+
   def password
     @password ||= Password.new(password_hash)
   end
